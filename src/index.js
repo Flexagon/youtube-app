b45c1827539ea91f23613ed4f0af53ee8c4a8757
Max Gavrosh <mmg@anadeainc.com>
2017-12-25 14:39:10 +0200

    Refactoring App is class component

--- src/index.js
+++ src/index.js
@@ -1,10 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
-const API_KEY = 'AIzaSyCiBtvIhoLjw5xdNv1nsAhbJaBlnGLVG0E'; 
+import SearchBar from './components/search-bar';
+import VideoList from './components/video-list';
 
-const App = () => {
-  return <div>React Starter</div>;
+const API_KEY = 'AIzaSyCiBtvIhoLjw5xdNv1nsAhbJaBlnGLVG0E';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { videos: [] };
+
+    YTSearch({key: API_KEY, term: 'surfboards'}, videos => {
+      this.setState({ videos });
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <VideoList videos={this.state.videos} />
+      </div>
+    );
+  }
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
