2577311de5c4c6ab8a1c613b1e683377455f1d01
Max Gavrosh <mmg@anadeainc.com>
2017-12-25 20:48:59 +0200

    Update VideoList component

--- src/components/video-list.js
+++ src/components/video-list.js
@@ -1,9 +1,20 @@
 import React from 'react';
 
+import VideoListItem from './video-list-item';
+
 const VideoList = props => {
-  return(
+  const videoItems = props.videos.map(video => {
+    return (
+      <VideoListItem
+        onVideoSelect={props.onVideoSelect}
+        key={video.etag}
+        video={video} />
+    );
+  });
+  
+  return (
     <ul className='col-md-4 list-group'>
-      {console.log(props)}
+      {videoItems}
     </ul>
   );
 }
